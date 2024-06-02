3d514f77819f7855d81ec6a5280f94e8982d17b6
salimaliyu <salemaliyu1992@gmail.com>
2024-06-02 11:46:26 +0000

    update on filters

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,18 +11,27 @@ PII_FIELDS = ("name", "email", "password", "ssn", "phone")
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Redacting Formatter class
+        """
+
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Redacting Formatter class
+        """
+
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
