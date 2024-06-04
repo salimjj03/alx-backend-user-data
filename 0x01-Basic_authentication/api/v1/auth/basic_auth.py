05468ae0466f10e25b7727c6b6077f5486757fa1
salimjj03 <salemaliyu1992@gmail.com>
2024-06-04 14:14:24 +0100

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,3 +29,23 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+
+        """
+        returns the decoded value of a Base64 strin
+        base64_authorization_header
+        """
+
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(
+                    base64_authorization_header
+                    ).decode("utf-8")
+        except Exception:
+            return None
