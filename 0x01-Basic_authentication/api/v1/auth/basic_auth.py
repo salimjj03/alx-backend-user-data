54b0ba4920847c81fd15997c77ea431751be7d98
salimjj03 <salemaliyu1992@gmail.com>
2024-06-04 14:28:00 +0100

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,3 +49,23 @@ class BasicAuth(Auth):
                     ).decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        returns the user email and password from the
+        Base64 decoded value.
+        """
+
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        user = decoded_base64_authorization_header.split(":")[0]
+        passwd = decoded_base64_authorization_header.split(":")[1]
+        return (user, passwd)
