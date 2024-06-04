57c75757c803f9179416b83ac4df5627de5ae7dd
salimjj03 <salemaliyu1992@gmail.com>
2024-06-04 12:59:03 +0100

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+BasicAuth that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth that inherits from Auth
+    """
+
+    pass
