020655b3b54804cca6162133be931dc441d9ff98
salimjj03 <salemaliyu1992@gmail.com>
2024-06-06 12:33:20 +0100

    cookir header

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Flask view that handles all routes for the Session
+authentication
+"""
+
+from api.v1.views import app_views
+from flask import jsonify, make_response, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route(
+        '/auth_session/login',
+        methods=['POST'],
+        strict_slashes=False
+        )
+def login():
+    """
+    login route
+    """
+
+    dic = request.form
+    if "email" not in dic or dic.get("email") is None:
+        return jsonify({"error": "email missing"}), 400
+
+    if "password" not in dic or dic.get("password") is None:
+        return jsonify({"error": "password missing"}), 400
+
+    user_name = dic.get("email")
+    passwd = dic.get("password")
+
+    user = User.search({"email": user_name})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if user[0].is_valid_password(passwd) is False:
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    user_dic = jsonify(user[0].to_json())
+    user_dic.set_cookie(getenv("SESSION_NAME"), session_id)
+    return user_dic
