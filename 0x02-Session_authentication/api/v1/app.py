b28b97e2a133d722b01fb2719abe4c12b2775e2e
salimjj03 <salemaliyu1992@gmail.com>
2024-06-06 10:01:15 +0100

    cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,9 +39,11 @@ def before_request():
                 '/api/v1/auth_session/login/'
                 ]
         if auth.require_auth(path, ls) is True:
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.session_cookie(request) is None:
+            if auth.authorization_header(
+                    request
+                    ) is None and auth.session_cookie(
+                            request
+                            ) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
