a5af9bfdb7c373d924d4398b320e200d9e54970d
salimjj03 <salemaliyu1992@gmail.com>
2024-06-12 11:29:05 +0100

    user auth

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,7 +50,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **dic: dict) -> User:
+    def find_user_by(self, **dic) -> User:
         """
         his method takes in arbitrary keyword arguments and
         returns the first row found in the users table as
