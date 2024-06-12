49bf2781f77a13bc3e313548df99c16db8e4f182
salimjj03 <salemaliyu1992@gmail.com>
2024-06-12 22:10:12 +0100

    session auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -105,3 +105,13 @@ class Auth:
             return user
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        takes a single user_id integer argument and returns None.
+
+        The method updates the corresponding user’s session ID
+        to None
+        """
+
+        self._db.update_user(user_id, session_id="None")
